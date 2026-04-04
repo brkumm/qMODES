afae082cb26dcc5a9b5acd5a7c1a68b926cca7b6
Bradley Kumm <brkumm@gmail.com>
2026-04-04 08:18:54 -0500

    refactored code to include use of environment vars.

---
+++ src/qMODES/computations_qmodes.py
@@ -0,0 +1,122 @@
+#-----------------------------------------------------------------------------
+# File:          computations_qmodes.py
+# Author:        Bradley Kumm (brkumm@gmail.com)
+# Last Modified: 2026/04/04 (YYYY/MM/DD)
+# Description:   Main function used to compute the modal moisture values
+#
+# Notes:         
+#               
+#------------------------------------------------------------------------------
+
+
+
+#------------------------------------------------------------------------------
+# IMPORTS
+
+import numpy    as np
+import xarray   as xa
+from   datetime import datetime
+
+from .read_environment_variables import get_QMODES_QKDATA_DIR, get_QMODES_QMODESDATA_DIR
+from .parameters   import nK, nplev, nlat, nlon
+from .templates    import template_qk_fname, template_qmodes_fname
+from .sample_files import 
+
+#------------------------------------------------------------------------------
+
+
+
+#--------------------------------------------------------------------------
+# MAIN COMPUTATION OF qmodes VALUES
+
+def compute_qmodes(mode, date, author_name=None, author_email=None):
+    """
+    Function that computes the moisture modal values from the qk 
+    (longitudnal Fourier components) that are stored in the corresponding 
+    qkdir files. 
+
+
+    REQUIRED INPUTS:
+        mode:         which mode is being computed (EIG, WIG, or ROT/BAL)
+        date:         date to perform the calculation for expressed as a 
+                      string in YYYYMMDD format
+
+    OPTIONAL INPUTS:
+        author_name:  name of author (stored in outputfile metadata)
+        author_email: email of author (stored in outputfile metadata)
+
+    IMPORTANT NOTE!!!
+        The factor of the background moisture derivative is left out of 
+        the qk and qmodes computations to have extra flexibility in how
+        to account for this term, latitude dependent vs indepent bkg 
+        etc... This factor needs to be accounted for before you will 
+        obtain correct moisture anomoly values. I recommend using the 
+        qMODES package data reader functions or at least looking at them
+        to see how this is done. 
+    """
+
+    #---------- Initial Variable Setup ----------
+    Mode_list = ['EIG','WIG','BAL']
+    grid_infile = sample_ERA_file()
+    qk_infile   = f"{get_QMODES_QKDATA_DIR()}/{template_qk_fname(date)}"
+
+
+    #---------- Reading Input Data ----------
+    grid_ds = xa.open_dataset(grid_infile)
+    lon     = grid_ds["lon"].values
+    qk_ds   = xa.open_dataset(qk_infile)
+    lat     = qk_ds["lat"].values
+    plev    = qk_ds["vgrid_int"].values
+
+
+    #---------- Main Loop ----------
+
+    dtnow = datetime.now()
+
+    for imode in Mode_list:
+    
+        #Reading in q_k fourier coefficient data
+        qk_ds = xa.open_dataset(qk_infile)
+        qk_mode = qk_ds[f"qk_{imode}"].values 
+        
+        #Initalizing q_mode
+        q_mode  = np.zeros((nplev, nlat, nlon))
+    
+        # Main Loop
+        for ilon in range(nlon):
+            
+            #k=0 (constant) term in fourier expansion
+            q_mode[:,:,ilon] += qk_mode[0,0,:,:]
+    
+            # k!= 0 terms of longitude fourier expansion or terms starting from the lower bound
+            for kk in range(1,nK):
+                q_mode[:,:,ilon] += 2.0 * ( qk_mode[0,kk,:,:] * np.cos(float(kk) * np.radians(lon[ilon])) - qk_mode[1,kk,:,:] * np.sin(float(kk) * np.radians(lon[ilon])) )
+    
+    
+    
+        # SAVING DATA TO NETCDF FILE
+    
+        coords    = {'plev': ( ['plev'], plev ),
+                     'lat' : ( ['lat' ], lat  ),
+                     'lon' : ( ['lon' ], lon  ) }
+    
+        data_vars = {f'q_{imode}' :([ 'plev', 'lat', 'lon'], q_mode,
+                            { 'long_name':f'{imode} Part of q'}) }
+    
+        attrs     = {'creation_date':dtnow.strftime("%m/%d/%Y, %H:%M:%S")}
+        
+        if author_name  != None: attrs['author'] = author_name,
+        if author_email != None: attrs['email' ] = author_email
+    
+        ds        = xa.Dataset(data_vars=data_vars,
+                               coords=coords,
+                               attrs=attrs)
+    
+    
+        outfile += f"{get_QMODES_QMODESDATA_DIR()}/{template_qmodes_fname(date)}"
+    
+    
+        ds.to_netcdf(outfile, mode='a')
+        print(f"q_{imode} data saved to:\n\t{outfile}")
+
+#--------------------------------------------------------------------------
