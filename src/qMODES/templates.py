04cd2035c40d5440bc21d05fd1a01cd9dc6b2282
Bradley Kumm <brkumm@gmail.com>
2026-04-28 15:30:30 -0500

    Added testdata filename templates to templates.py module.

--- src/qMODES/templates.py
+++ src/qMODES/templates.py
@@ -16,6 +16,10 @@
 #--------------------------------------------------------------------------
 # FILE NAMING TEMPLATES
 
+# k_str variables should be a string of length 3 (padd with zeros in front)
+# date variables should be string with format 'YYYYMMDD'
+
+# Input data file names
 def template_vsf_fname():
 	return "vsf.data.nc"
 
@@ -23,28 +27,32 @@ def template_vsf_int_fname():
 	return "vsf_int.data.nc"
 
 def template_hough_fname(k_str):
-	# k_str should be a string with three numbers
-	# leading 0's if need be
 	return f"hough_F320_M60.wn00{k_str}.nc"
 
 def template_coef_fname(date):
-	#YYYYMMDD format
 	return f"Hough_coeff_M60_F320_{date}0000000.nc"
 
 def template_freq_fname(k_str):
 	return f"freq_F320_M60.data.wn00{k_str}"
 
+#Output Data filenames and patterns
 def template_qk_fname(date):
 	return f"qk_{date}0000000.nc"
-
-def template_qmodes_fname(date):
-	return f"qmodes_{date}0000000.nc"
 
 def template_qk_with_klb_kub_ktot_fname(date, klb_str, kub_str, ktot_str):
 	return f"qk_{date}0000000_klb-{klb_str}_kub-{kub_str}_ktot-{ktot_str}.nc"
 
+def template_combine_qk_file_pattern(date, ktot_str):
+    return f"qk_{date}0000000_klb-*_kub-*_ktot-{ktot_str}.nc"
+
+def template_qmodes_fname(date):
+	return f"qmodes_{date}0000000.nc"
+
 def template_qmodes_with_klb_kub_ktot_fname(date, klb_str, kub_str, ktot_str):
 	return f"qmodes_{date}0000000_klb-{klb_str}_kub-{kub_str}_ktot-{ktot_str}.nc"
+
+def template_combine_qmodes_file_pattern(date, ktot_str):
+    return f"qmodes_{date}0000000_klb-*_kub-*_ktot-{ktot_str}.nc"
 
 def template_ERA_fname(date):
 	return f"ERA5_{date}_q-t_pl_data.nc"
@@ -52,10 +60,14 @@ def template_ERA_fname(date):
 def template_ERA_uv_fname(date):
 	return f"ERA5_{date}_u-v_pl_data.nc"
 
-def template_combine_qk_file_pattern(date, ktot_str):
-    return f"qk_{date}0000000_klb-*_kub-*_ktot-{ktot_str}.nc"
+# Test data filenames
+def template_testdata_vsf_fname():
+	return"testdata_vsf.nc"
 
-def template_combine_qmodes_file_pattern(date, ktot_str):
-    return f"qmodes_{date}0000000_klb-*_kub-*_ktot-{ktot_str}.nc"
+def template_testdata_hough_fname(k_str):
+	return"testdata_hough_{k_str}.nc"
+
+def template_testdata_coef_fname():
+	return"testdata_coef.nc"
 
 #--------------------------------------------------------------------------
