2268033289891ea67424cfb6280552bb2c45dd12
Bradley Kumm <brkumm@gmail.com>
2026-05-14 15:28:35 -0500

    adding type hinting in for function outputs.

--- src/qMODES/get_environment_variables.py
+++ src/qMODES/get_environment_variables.py
@@ -23,27 +23,27 @@ import os
 # FUNCTIONS THAT READ IN ENV VARS
 
 # functions to retrieve directories
-def get_qMODES_INPUT_DATA_DIR():
+def get_qMODES_INPUT_DATA_DIR() -> str:
     return os.getenv("QMODES_INPUT_DATA_DIR")
 
-def get_qMODES_OUTPUT_DATA_DIR():
+def get_qMODES_OUTPUT_DATA_DIR() -> str:
     return os.getenv("QMODES_OUTPUT_DATA_DIR")
 
-def get_qMODES_TEST_INPUT_DATA_DIR():
+def get_qMODES_TEST_INPUT_DATA_DIR() -> str:
     return os.getenv("QMODES_TEST_INPUT_DATA_DIR")
 
-def get_qMODES_TEST_OUTPUT_DATA_DIR():
+def get_qMODES_TEST_OUTPUT_DATA_DIR() -> str:
     return os.getenv("QMODES_TEST_OUTPUT_DATA_DIR")
 
 # Functinos to retrieve parameter files
-def get_QMODES_PARAMETERS_FILE():
+def get_QMODES_PARAMETERS_FILE() -> str:
     return os.getenv("QMODES_PARAMETERS_FILE")
 
-def get_QMODES_TEST_PARAMETERS_FILE():
+def get_QMODES_TEST_PARAMETERS_FILE() -> str:
     return os.getenv("QMODES_TEST_PARAMETERS_FILE")
 
 # Other ENV Vars
-def get_QMODES_SUPPRESS_NEW_USER_WARNINGS():
+def get_QMODES_SUPPRESS_NEW_USER_WARNINGS() -> bool:
     if os.getenv("QMODES_SUPPRESS_NEW_USER_WARNINGS") == "True":
         return True
     else:
