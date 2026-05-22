74c60a7e51c97f08d7a796a7c41b6da5c4d1eef8
Bradley Kumm <brkumm@gmail.com>
2026-05-22 14:08:34 -0500

    Refactoring for new env var naming shceme.

--- calculation_scripts/aggregate_qk_data.py
+++ calculation_scripts/aggregate_qk_data.py
@@ -1,13 +1,24 @@
 #-----------------------------------------------------------------------------
 # IMPORTS
-from qMODES import get_QMODES_QKDATA_DIR
-from qMODES import nK
-from qMODES import get_klb_kub_ktot_from_qk_filename, get_qk_files_with_date_and_ktot
+from qMODES import get_QMODES_OUTPUT_DATA_DIR, get_QMODES_PARAMETERS_FILE
+from qMODES import get_qk_files_with_date_and_ktot
 from qMODES import check_qk_files_have_all_modes, check_qk_files_cover_ktot_range, combine_qk_files_from_list
 from qMODES import template_qk_fname
 
+import os
+import yaml
 import argparse
-import xarray as xa
+#-----------------------------------------------------------------------------
+
+
+
+#-----------------------------------------------------------------------------
+# IMPORTING DEFAULT ktot VALUE FROM PARAMETER FILE
+with open(get_QMODES_PARAMETERS_FILE(), 'r') as param_file:
+    params = yaml.safe_load(param_file)
+
+default_ktot = params['mode_parameters']['nK']
+
 #-----------------------------------------------------------------------------
 
 
@@ -16,8 +27,8 @@ import xarray as xa
 # READING COMMAND LINE ARGUMENTS USING argparse
 parser = argparse.ArgumentParser(description='This script is used to aggregate qk datafiles that are from the same date but cover different k values.')
 parser.add_argument('-d','--date', help='Date to comput the qk values for', required=True)
-parser.add_argument('--ktot', help='Total number of k values', type=int, default=nK  )
-parser.add_argument('--rm_old', help='include to remove old files after creating aggregate file', type=int, default=nK )
+parser.add_argument('--ktot', help='Total number of k values', type=int, default=default_ktot  )
+parser.add_argument('--rm_old', help='include to remove old files after creating aggregate file', action='store_true')
 
 args   = parser.parse_args()
 date   = args.date
@@ -31,27 +42,20 @@ rm_old = args.rm_old
 #-----------------------------------------------------------------------------
 # MAIN 
 
-# Getting all files qk files in QMODES_QKDATA_DIR that have date and ktot
+# Getting all qk files in QMODES_QKDATA_DIR that have date and ktot
 # specified from argparse arguments.
 
 ktot_str = "0"*(3-len(str(ktot))) + str(ktot)
 
 file_list = get_qk_files_with_date_and_ktot(date, ktot_str)
-combined_outfile = f"{get_QMODES_QKDATA_DIR()}/{template_qk_fname(date)}" 
+combined_outfile = template_qk_fname(get_QMODES_OUTPUT_DATA_DIR(), date) 
 
-#for fname in file_list:
-#    print(fname)
 
 # Check if file_list covers all of the k values
-
-#print(check_qk_files_cover_ktot_range(file_list))
-
-cover_ktot_range = check_qk_files_cover_ktot_range(file_list, 4) # 4 added for testing
+cover_ktot_range = check_qk_files_cover_ktot_range(file_list, ktot)
 files_have_all_modes = check_qk_files_have_all_modes(file_list)
 
 if cover_ktot_range and files_have_all_modes:
-    #aggregate_ds = xa.open_mfdataset(file_list, combine="by_coords")
-    #aggregate_ds.to_netcdf( f"{get_QMODES_QKDATA_DIR()}/{template_qk_fname(date)}" )
     combine_qk_files_from_list(file_list, combined_outfile)
 
 elif not cover_ktot_range:
@@ -59,5 +63,12 @@ elif not cover_ktot_range:
 
 elif not files_have_all_modes:
     print("ERROR: all files must contain EIG, WIG, and BAL modes.")
+
+if rm_old:
+    print("THE FOLLOWING FILES HAVE BEEN REMOVED:")
+    for fname in file_list:
+        os.remove(fname)
+        print(f"\t{fname}")
+
 
 #-----------------------------------------------------------------------------
