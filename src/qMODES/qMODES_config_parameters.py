686c601d83ce9b028933ce07f27aafde5c3255f1
Brad <bkumm@Graces-MacBook-Pro.local>
2026-09-06 16:55:48 -0500

    Added default path for ERA to 'get_default_data_dir' method, and updated default paths for qk and qmodes to qk_data and qmodes_data respectively.

--- src/qMODES/qMODES_config_parameters.py
+++ src/qMODES/qMODES_config_parameters.py
@@ -34,16 +34,47 @@ class qMODES_config_parameters:
 
         # Adding base dir to input and output data directories if they are relative paths
         if isinstance(self.input_data_dir, str) and repo_dir not in self.input_data_dir:
-            self.input_data_dir = os.path.join(repo_dir, self.input_data_dir)
+            temp_path = os.path.join(repo_dir, self.input_data_dir)
+            #clean up the path using os.path.normpath before updating input_data_dir
+            self.input_data_dir = os.path.normpath(temp_path)
+
         if isinstance(self.output_data_dir, str) and repo_dir not in self.output_data_dir:
-            self.output_data_dir = os.path.join(repo_dir, self.output_data_dir)
+            temp_path = os.path.join(repo_dir, self.output_data_dir)
+            #clean up the path using os.path.normpath before updating output_data_dir
+            self.output_data_dir = os.path.normpath(temp_path)
 
+    # Method to retrieve the default directory location for various model inputs and outputs
+    def get_default_data_dir(self, data_type: str) -> str:
+        """
+        Returns the qMODES default directory location for various model inputs and outputs using the input_data_dir and output_data_dir values.
+        Valid inputs for 'data_type' string are: 'ERA', 'coef', 'hough', 'freq', 'vsf', 'vsf_int', 'qk', 'qmodes', everything else will raise a ValueError.
+        NOTE: By default the frequency (freq) data is stored hough directory, and vsf_int data is stored vsf directory.
+        """
+
+        data_type_to_dict_map = {
+            "ERA": f"{self.input_data_dir}/ERA_data/ERA/",
+            "coef": f"{self.input_data_dir}/MODES_data/coef/",
+            "hough": f"{self.input_data_dir}/MODES_data/hough/",
+            "freq": f"{self.input_data_dir}/MODES_data/hough/",
+            "vsf": f"{self.input_data_dir}/MODES_data/vsf/",
+            "vsf_int": f"{self.input_data_dir}/MODES_data/vsf/",
+            "qk": f"{self.output_data_dir}/qk_data/",
+            "qmodes": f"{self.output_data_dir}/qmodes_data/"
+        }
+
+        # raise error if the input data_type is not valid
+        if data_type not in data_type_to_dict_map:
+            raise ValueError(f"Input value for 'data_type' is not valid. Valid inputs are: {list(data_type_to_dict_map.keys())}")
+
+        # Clean up the path using os.path.normpath
+        return os.path.normpath(data_type_to_dict_map[data_type])
+    
 #-----------------------------------------------------------------------------
 
 
 
 #-----------------------------------------------------------------------------
-# FUNCTION TO LOAD qMODES_config_parameters FROM A YAML FILE
+# FUNCTION TO LOAD qMODES_config_parameters FROM A GIVEN YAML FILE
 
 def load_qmodes_config(file_path: str) -> qMODES_config_parameters:
     """Reads a YAML configuration file and returns a qMODES_config_parameters 
