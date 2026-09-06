06c1cfee57e4bf97b8502cbaff9c16602c0aa3e7
Brad <bkumm@Graces-MacBook-Pro.local>
2026-09-06 11:40:09 -0500

    Reorganizing to the new directory structure.

---
+++ setup_qMODES.sh
@@ -0,0 +1,5 @@
+# Adding qMODES parent directory to PYTHONPATH
+export PYTHONPATH="${PYTHONPATH}:${QMODES_REPO_DIR}/src/"
+
+# starting qMODES virtual environment
+source $QMODES_REPO_DIR/qMODES_venv/bin/activate
